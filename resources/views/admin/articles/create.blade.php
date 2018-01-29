3f250a43a0945bd8a5b91a7b9e3368365fc72c8a
Ricardo-MSS <ing.ricardosuarezc@gmail.com>
2018-01-29 11:51:55 -0500

    Se elimina el plugin jQueryFileUpload

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -4,39 +4,39 @@
 <h1>Crear Artículo</h1>
 
 <div class="row">
-	<div class="col-md-4">
-		{!! Form::open(['route' => 'articles.store', 'method' => 'POST', 'files' => 'TRUE']) !!}
+	<div class="col-lg-6">
+		{!! Form::open(['route' => 'articles.store', 'method' => 'POST', 'files' => 'TRUE', 'class' => 'dropzone']) !!}
 
-	<div class="form-group">
-		{!! Form::label('category_id','Categoria') !!}
-		{!! Form::select('category_id', $categories, null, ['class' => 'form-control select_category', 'placeholder' => 'Seleccione una categoría']) !!}
-	</div>
+			<div class="form-group">
+				{!! Form::label('category_id','Categoria') !!}
+				{!! Form::select('category_id', $categories, null, ['class' => 'form-control select_category', 'placeholder' => 'Seleccione una categoría']) !!}
+			</div>
 
-	<div class="form-group">
-		{!! Form::label('title','Titulo') !!}
-		{!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Titulo de la nota']) !!}
-	</div>
+			<div class="form-group">
+				{!! Form::label('title','Titulo') !!}
+				{!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Titulo de la nota']) !!}
+			</div>
 
-	<div class="form-group">
-		{!! Form::label('content','Contenido') !!}
-		{!! Form::textarea('content', null, ['class' => 'form-control textarea-content']) !!}
-	</div>
+			<div class="form-group">
+				{!! Form::label('content','Contenido') !!}
+				{!! Form::textarea('content', null, ['class' => 'form-control textarea-content']) !!}
+			</div>
 
-	<div class="form-group">
-		{!! Form::label('image','Imagen') !!}
-		{!! Form::file('image', null, ['class' => 'form-control']) !!}
-	</div>	
+		    <div class="form-group">
+				{!! Form::label('image','Imagen') !!}
+				{!! Form::file('image[]', null, ['class' => 'form-control', 'multiple']) !!}
+			</div>
 
-	<div class="form-group">
-		{!! Form::label('tags','Tag') !!}
-		{!! Form::select('tags[]', $tags, null, ['class' => 'form-control select_tags', 'multiple']) !!}
-	</div>
+			<div class="form-group">
+				{!! Form::label('tags','Tag') !!}
+				{!! Form::select('tags[]', $tags, null, ['class' => 'form-control select_tags', 'multiple']) !!}
+			</div>
 
-	<div class="form-group">
-		{!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
-	</div>
+			<div class="form-group">
+				{!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+			</div>
 
-{!! Form::close() !!}	
+		{!! Form::close() !!}
 	</div>
 
 </div>
@@ -57,6 +57,5 @@
 
 	$('.textarea-content').trumbowyg();
 
-</script>
-	
+</script>	
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,13 +30,12 @@
     {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
     <!-- Latest compiled and minified JavaScript -->
     <!-- jquery -->
-    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <!-- [/] jquery -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="{{ asset('plugins/chosen_v1.8.3/chosen.jquery.js') }}"></script>
     <script src="{{ asset('plugins/trumbowyg/trumbowyg.js') }}"></script>
-
     @yield('js');
 </body>
 </html>
